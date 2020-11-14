1ed41f211587130469bafe1c39ff7ed87c8ecb31
NE <nouni.elbachir@gmail.com>
2020-11-14 23:32:32 +0100

    add new hotels rest api

--- src/main/java/com/example/mysqlmongodb/backend/repositories/HotelRepository.java
+++ src/main/java/com/example/mysqlmongodb/backend/repositories/HotelRepository.java
@@ -1,9 +1,28 @@
 package com.example.mysqlmongodb.backend.repositories;
 
 import com.example.mysqlmongodb.backend.mongo.Hotel;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Collections;
+import java.util.List;
+
 @Repository
 public interface HotelRepository extends MongoRepository<Hotel, String> {
+    List<Hotel> findByPricePerNightGreaterThan(int value);
+
+    List<Hotel> findByPricePerNightLessThan(int value);
+
+    Page<Hotel> findByPricePerNightGreaterThan(int value, Pageable pageable);
+
+    Page<Hotel> findByPricePerNightLessThan(int value, Pageable pageable);
+
+    List<Hotel> filterByReviewsRatingGreaterThan(int value);
+
+    default List<Hotel> filterByReviewsRatingLessThan(int value) {
+        //here we will use query dsl
+        return Collections.emptyList();
+    }
 }
--- src/main/java/com/example/mysqlmongodb/controllers/rest/HotelController.java
+++ src/main/java/com/example/mysqlmongodb/controllers/rest/HotelController.java
@@ -4,13 +4,20 @@ import com.example.mysqlmongodb.backend.entity.Departement;
 import com.example.mysqlmongodb.backend.entity.User;
 import com.example.mysqlmongodb.backend.mongo.Hotel;
 import com.example.mysqlmongodb.backend.repositories.HotelRepository;
+import com.sun.istack.NotNull;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Collections;
 import java.util.List;
+
+import static java.util.Collections.emptyList;
 
 @RestController
 @RequestMapping("hotels")
@@ -23,9 +30,44 @@ public class HotelController {
         return hotelRepository.findAll();
     }
 
+    @GetMapping("/paged")
+    public Page<Hotel> allPaged() {
+        return hotelRepository.findAll(Pageable.unpaged());
+    }
+
     @GetMapping("/{hotelId}")
     public Hotel hotel(@PathVariable("hotelId") String id) {
         return hotelRepository.findById(id)
                 .orElse(null);
+    }
+
+    @GetMapping("/filter-by-price/{cmp}/{value}")
+    public List<Hotel> hotelsByPrices(@NotNull @PathVariable("cmp") String cmp, @PathVariable("value") int value) {
+        if ("gt".equalsIgnoreCase(cmp))
+            return hotelRepository.findByPricePerNightGreaterThan(value);
+        else if ("lt".equalsIgnoreCase(cmp))
+            return hotelRepository.findByPricePerNightLessThan(value);
+        else
+            return emptyList();
+    }
+
+    @GetMapping("/filter-by-rating/{cmp}/{value}")
+    public List<Hotel> hotelsByRating(@NotNull @PathVariable("cmp") String cmp, @PathVariable("value") int value) {
+        if ("gt".equalsIgnoreCase(cmp))
+            return hotelRepository.filterByReviewsRatingGreaterThan(value);
+        else if ("lt".equalsIgnoreCase(cmp))
+            return hotelRepository.filterByReviewsRatingLessThan(value);
+        else
+            return emptyList();
+    }
+
+    @GetMapping("/paged/filter-by-price/{cmp}/{value}")
+    public Page<Hotel> hotelsByPricesPaged(@NotNull @PathVariable("cmp") String cmp, @PathVariable("value") int value) {
+        if ("gt".equalsIgnoreCase(cmp))
+            return hotelRepository.findByPricePerNightGreaterThan(value, Pageable.unpaged());
+        else if ("lt".equalsIgnoreCase(cmp))
+            return hotelRepository.findByPricePerNightLessThan(value, Pageable.unpaged());
+        else
+            return Page.empty();
     }
 }
