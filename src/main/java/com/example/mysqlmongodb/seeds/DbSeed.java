105b1fb7e2e4de48e8568dc81f3571da3a70404e
NE <nouni.elbachir@gmail.com>
2020-11-14 23:11:05 +0100

    add test data seeder

---
+++ src/main/java/com/example/mysqlmongodb/seeds/DbSeed.java
@@ -0,0 +1,96 @@
+package com.example.mysqlmongodb.seeds;
+
+import com.example.mysqlmongodb.backend.entity.Departement;
+import com.example.mysqlmongodb.backend.entity.User;
+import com.example.mysqlmongodb.backend.mongo.Hotel;
+import com.example.mysqlmongodb.backend.mongo.embeddables.Address;
+import com.example.mysqlmongodb.backend.mongo.embeddables.Reservation;
+import com.example.mysqlmongodb.backend.mongo.embeddables.Reviews;
+import com.example.mysqlmongodb.backend.repositories.DepartementRepository;
+import com.example.mysqlmongodb.backend.repositories.HotelRepository;
+import com.example.mysqlmongodb.backend.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.Collections;
+
+import static java.util.Arrays.asList;
+import static java.util.Collections.singletonList;
+
+@Component
+@RequiredArgsConstructor
+public class DbSeed implements CommandLineRunner {
+    private final DepartementRepository departementRepository;
+    private final UserRepository userRepository;
+    private final HotelRepository hotelRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        hotelRepository.deleteAll();
+        userRepository.deleteAll();
+        departementRepository.deleteAll();
+
+        Departement it = Departement.builder()
+                .titre("IT")
+                .build();
+
+        departementRepository.save(it);
+        User admin = User.builder()
+                .username("admin")
+                .departement(it)
+                .build();
+
+        userRepository.save(admin);
+
+        Hotel sofitel = Hotel.builder()
+                .pricePerNight(200)
+                .address(
+                        Address.builder()
+                                .city("Paris")
+                                .country("France")
+                                .build()
+                )
+                .reviews(asList(
+                        Reviews.builder()
+                                .rating(7)
+                                .comment("Nothing")
+                                .approved(false)
+                                .build(),
+                        Reviews.builder()
+                                .rating(10)
+                                .comment("Good")
+                                .approved(true)
+                                .build()
+                ))
+                .reservations(singletonList(
+                        Reservation.builder()
+                                .userId(admin.getId())
+                                .date(LocalDate.now().plusDays(10))
+                                .nbrPersonnes(1)
+                                .build()
+                ))
+                .build();
+
+        Hotel ibis = Hotel.builder()
+                .pricePerNight(80)
+                .address(
+                        Address.builder()
+                                .city("Rabat")
+                                .country("Maroc")
+                                .build()
+                )
+                .reviews(singletonList(
+                        Reviews.builder()
+                                .rating(7)
+                                .comment("Nothing")
+                                .approved(false)
+                                .build()
+                ))
+                .build();
+
+        hotelRepository.saveAll(asList(sofitel, ibis));
+    }
+}
