b634516fbae669474c33a767ef85a302c0e7fbf4
NE <nouni.elbachir@gmail.com>
2020-11-14 23:08:30 +0100

    create project

---
+++ src/main/java/com/example/mysqlmongodb/MysqlMongodbApplication.java
@@ -0,0 +1,13 @@
+package com.example.mysqlmongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysqlMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MysqlMongodbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mysqlmongodb/MysqlMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mysqlmongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MysqlMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
